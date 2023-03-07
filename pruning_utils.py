ef6cea59ff532539135cfe694c7dc96f7e8b27af
kiaraesguerra <kiaraesguerra@gmail.com>
2023-03-07 19:24:36 +0800

    pruning methods args

--- pruning_utils.py
+++ pruning_utils.py
@@ -3,24 +3,24 @@ from cnn_sao_utils import *
 
 
 def custom_pruner_degree(model, args):
-    if pruning_method == 'SAO':
+    if args.pruning_method == 'SAO':
         model = Delta_SAO_same_deg(model, args.degree, args.gain)
-    elif pruning_method == 'SAO-relu':
+    elif args.pruning_method == 'SAO-relu':
         model = Delta_ReLU_SAO_same_deg(model,  args.degree, args.gain)
-    elif pruning_method == 'LMP-S':
+    elif args.pruning_method == 'LMP-S':
         model = lmp_same_deg(model,  args.degree)
-    elif pruning_method == 'RG-S':
+    elif args.pruning_method == 'RG-S':
         model = d_regular_pruning_same_deg(model,  args.degree)      
     return model
 
 def custom_pruner(model, args):
-    if pruning_method == 'SAO':
+    if args.pruning_method == 'SAO':
         model = Delta_SAO(model, args.sparsity, args.gain)
-    elif pruning_method == 'SAO-relu':
+    elif args.pruning_method == 'SAO-relu':
         model = Delta_ReLU_SAO(model, args.sparsity, args.gain)
-    elif pruning_method == 'LMP-S':
+    elif args.pruning_method == 'LMP-S':
         model = lmp(model, args.sparsity)
-    elif pruning_method == 'RG-S':
+    elif args.pruning_method == 'RG-S':
         model = d_regular_pruning(model, args.sparsity)      
     return model
 
